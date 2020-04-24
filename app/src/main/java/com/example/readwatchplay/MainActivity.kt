3eaea41b6e092e8b301c9f9b6ea3f265d6c3ee6f
colunira <jul.iskierka@gmail.com>
2020-04-24 13:17:12 +0200

    Google auth login

--- app/src/main/java/com/example/readwatchplay/MainActivity.kt
+++ app/src/main/java/com/example/readwatchplay/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.readwatchplay
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +9,9 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,5 +27,45 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
+        // Authentication
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN)
+
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+
+
+    companion object {
+
+        private const val RC_SIGN_IN = 123
     }
 }
--- app/src/main/java/com/example/readwatchplay/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/readwatchplay/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.readwatchplay.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +9,11 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.example.readwatchplay.MainActivity
 import com.example.readwatchplay.R
+import com.firebase.ui.auth.AuthUI
+import kotlinx.android.synthetic.main.fragment_home.*
+
 
 class HomeFragment : Fragment() {
 
@@ -26,6 +31,26 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
         return root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        logoutButton.setOnClickListener{
+            AuthUI.getInstance()
+                .signOut(context!!)
+                .addOnCompleteListener {
+                    startActivity(
+                        Intent(
+                            activity,
+                            MainActivity::class.java
+                        )
+                    )
+
+                    activity!!.finish()
+                }
+        }
     }
 }
